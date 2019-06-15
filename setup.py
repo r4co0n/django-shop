9c852f5d5ab34c9ac5637be666f474750714e877
Simon Hollenbach <r4co0n@ulfred.home.co0n>
2019-06-15 20:57:50 +0200

    setup: Fix error reading README.md file, declare encoding
    Before this commit, pip would fail in running egg_info, outputting:
    ```
        ERROR: Complete output from command python setup.py egg_info:
        ERROR: Traceback (most recent call last):
          File "<string>", line 1, in <module>
          File "/tmp/pip-install-f5b6c4c4/django-shop/setup.py", line 9, in <module>
            long_description = fh.read()
          File "/opt/mysite/venv/lib/python3.5/encodings/ascii.py", line 26, in decode
            return codecs.ascii_decode(input, self.errors)[0]
        UnicodeDecodeError: 'ascii' codec can't decode byte 0xe2 in position 1019: ordinal not in range(128)
        ----------------------------------------
    ERROR: Command "python setup.py egg_info" failed with error code 1 in /tmp/pip-install-f5b6c4c4/django-shop/
    ```
    
    We now declare the encoding of the README.md file as `utf-8`. This was
    introduced when using long dashes in the headings, i suspect in
    8f93af20f1c967cc4c6af6f1bad1ccefc94e64d9 .

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 from setuptools import setup, find_packages
 import shop
 
-with open('README.md', 'r') as fh:
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 CLASSIFIERS = [
